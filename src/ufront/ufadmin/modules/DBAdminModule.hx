9f96e68200fb5f5cd210edd9de34733bb04fa163
Jason O'Neil <jason.oneil@gmail.com>
2014-11-25 14:52:03 +0800

    Error message if there is no database connection

--- src/ufront/ufadmin/modules/DBAdminModule.hx
+++ src/ufront/ufadmin/modules/DBAdminModule.hx
@@ -8,17 +8,22 @@ class DBAdminModule extends UFAdminModule
 	public function new() {
 		super( "db", "Database Admin" );
 	}
-
+	
 	public function checkPermissions() {
 		return true; // Set up more fine grained permissions later...
 	}
-
+	
 	@:route("/*")
-	public function doDefault()
-	{
-		spadm.AdminStyle.BASE_URL = "/ufadmin/db/";
-		ufront.spadm.DBAdmin.handler("/ufadmin/db/");
-		context.completion.set( CRequestHandlersComplete );
-		context.completion.set( CFlushComplete );
+	public function doDefault() {
+		if ( sys.db.Manager.cnx==null ) {
+			return 'No Database Connection Found';
+		}
+		else {
+			spadm.AdminStyle.BASE_URL = baseUri;
+			ufront.spadm.DBAdmin.handler(baseUri);
+			context.completion.set( CRequestHandlersComplete );
+			context.completion.set( CFlushComplete );
+			return null;
+		}
 	}
 }
