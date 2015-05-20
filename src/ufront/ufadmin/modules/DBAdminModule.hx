266f95f3589e2784f199f03b9ba128a8f2eaeb24
Jason O'Neil <jason.oneil@gmail.com>
2015-05-20 21:26:44 +0800

    Get rid of defunct ufront.web.Dispatch import.

--- src/ufront/ufadmin/modules/DBAdminModule.hx
+++ src/ufront/ufadmin/modules/DBAdminModule.hx
@@ -1,18 +1,17 @@
 package ufront.ufadmin.modules;
 
 import ufront.ufadmin.UFAdminModule;
-import ufront.web.Dispatch;
 
 class DBAdminModule extends UFAdminModule
 {
 	public function new() {
 		super( "db", "Database Admin" );
 	}
-	
+
 	public function checkPermissions() {
 		return true; // Set up more fine grained permissions later...
 	}
-	
+
 	@:route("/*")
 	public function doDefault() {
 		#if server
